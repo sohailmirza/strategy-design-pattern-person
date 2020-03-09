f420ec7ab852399105198ffcbb20c84862cf84e8
sohailmirza <sohailabbas1000@gmail.com>
2020-03-09 23:47:42 +0530

    modifies ShotGun class

--- src/com/strategy/person/ShotGun.java
+++ src/com/strategy/person/ShotGun.java
@@ -1,8 +1,12 @@
 package com.strategy.person;
 
 public class ShotGun implements IAttackBehavior {
+    int damage = 200;
     @Override
     public String attack() {
         return "Uses Shot Gun to attack";
+    }
+    public String damage() {
+        return "incurs damage of 200 HP";
     }
 }
