6f96d844bd64cb764c193f2b3b89eeb3d01e1c2c
sohailmirza <sohailabbas1000@gmail.com>
2020-03-09 23:38:17 +0530

    adds Main class

--- src/com/strategy/person/Main.java
+++ src/com/strategy/person/Main.java
@@ -1,4 +1,8 @@
 package com.strategy.person;
 
 public class Main {
+    public static void main(String[] args) {
+        Person soldier_1 = new Soldier("Geralt");
+        System.out.println("Soldier " + soldier_1.getName() + " " + soldier_1.wayOfAttack());
+    }
 }
