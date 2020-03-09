a6aeb1cf4f92465ff644c073e6e3918ebfe0b540
sohailmirza <sohailabbas1000@gmail.com>
2020-03-09 23:17:34 +0530

    modifies Person.java

--- src/com/strategy/person/Person.java
+++ src/com/strategy/person/Person.java
@@ -1,4 +1,18 @@
 package com.strategy.person;
 
-public class Person {
+public class Person implements IAttackBehavior{
+    private String name;
+    public IAttackBehavior attackBehavior;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String wayOfAttack() {
+        return attackBehavior.attack();
+    }
 }
