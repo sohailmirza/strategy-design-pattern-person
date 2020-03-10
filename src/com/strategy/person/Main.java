612a36a1e6edfb884ec40a52733da4260fbcf278
sohailmirza <sohailabbas1000@gmail.com>
2020-03-10 23:03:00 +0530

    Modifies Main class

--- src/com/strategy/person/Main.java
+++ src/com/strategy/person/Main.java
@@ -2,10 +2,9 @@ package com.strategy.person;
 
 public class Main {
     public static void main(String[] args) {
-        Person soldier_1 = new Soldier("Geralt");
-        Person soldier_2 = new Soldier("Yennefer");
-//        soldier_2.setAttackBehavior(new AssaultRifle(500));
-        soldier_2.setAttackBehavior(new AssaultRifle());
+        Person soldier_1 = new Soldier("Akhil");
+        Person soldier_2 = new Soldier("Aman");
+        soldier_2.setAttackBehavior(new AssaultRifle(500));
         System.out.println("Soldier " + soldier_1.getName() + " " + soldier_1.wayOfAttack() + " and "
                 + soldier_1.amountOfDamage());
         System.out.println("Soldier " + soldier_2.getName() + " " + soldier_2.wayOfAttack() + " and "
