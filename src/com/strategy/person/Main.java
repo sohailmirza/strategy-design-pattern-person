6ab8bff86cbc44a84da43863931194cb8304bdd1
sohailmirza <sohailabbas1000@gmail.com>
2020-03-10 19:47:03 +0530

    Modifies Main class

--- src/com/strategy/person/Main.java
+++ src/com/strategy/person/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person soldier_1 = new Soldier("Geralt");
         Person soldier_2 = new Soldier("Yennefer");
+//        soldier_2.setAttackBehavior(new AssaultRifle(500));
         soldier_2.setAttackBehavior(new AssaultRifle());
         System.out.println("Soldier " + soldier_1.getName() + " " + soldier_1.wayOfAttack() + " and "
                 + soldier_1.amountOfDamage());
