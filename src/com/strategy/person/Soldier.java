e49049b99939415f49f8544852a9699b1e70d524
sohailmirza <sohailabbas1000@gmail.com>
2020-03-10 23:02:50 +0530

    Modifies Soldier class

--- src/com/strategy/person/Soldier.java
+++ src/com/strategy/person/Soldier.java
@@ -1,8 +1,15 @@
 package com.strategy.person;
 
 public class Soldier extends Person {
+    private String defenseArea;
     public Soldier(String name) {
         super(name);
         attackBehavior = new ShotGun();
+    }
+    public void setDefenseArea(String defenseArea) {
+        this.defenseArea = defenseArea;
+    }
+    public String getDefenseArea() {
+        return defenseArea;
     }
 }
